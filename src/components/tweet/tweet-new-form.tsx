1e5fd6ce815febdb21912e6f0ef73006fed0384e
Sergio Castelani <sergio.castelani@gmail.com>
2024-02-03 12:13:05 -0300

    feat: loading feedback on tweet submition

--- src/components/tweet/tweet-new-form.tsx
+++ src/components/tweet/tweet-new-form.tsx
@@ -3,6 +3,8 @@ import { Modal } from "../modal";
 import { styled } from "styled-components";
 import { ApiTweet } from "../../api/api-tweet";
 import { isAxiosError } from "axios";
+import { useState } from "react";
+import { PulseLoader } from "react-spinners";
 
 export interface TweetNewFormProps
 {
@@ -12,17 +14,24 @@ export interface TweetNewFormProps
 
 export function TweetNewForm(props: TweetNewFormProps)
 {
+    const [loading, setLoading] = useState(false);
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = async (formData: any) => 
     {
+        if (loading)
+            return;
+
         try 
         {
+            setLoading(true);
             await (new ApiTweet()).create(formData.content);
+            setLoading(false);
             props.onCreated();
         } 
         catch (error: any) 
         {
+            setLoading(false);
             if(error.response?.data?.message) {
                 alert(`Error: ${error.response?.data?.message}`);
                 return;
@@ -41,7 +50,9 @@ export function TweetNewForm(props: TweetNewFormProps)
     const handleCancel = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
     {
         event.preventDefault();
+        setLoading(true);
         props.onCancel();
+        setLoading(false);
     }
     
     return (
@@ -63,8 +74,11 @@ export function TweetNewForm(props: TweetNewFormProps)
                 }
             
                 <Footer>
-                    <Cancel className="btn" onClick={handleCancel}>Cancel</Cancel>
-                    <Submit className="btn">Submit</Submit>
+                    <Cancel className="btn" onClick={handleCancel} disabled={loading}>Cancel</Cancel>
+                    <Submit className="btn" disabled={loading}>
+                        {loading || 'Submit'}
+                        <PulseLoaderStyled loading={loading} size="0.5rem" color="white"/>
+                    </Submit>
                 </Footer>
             </Form>
         </Modal>
@@ -115,7 +129,16 @@ const Submit = styled.button`
     background-color: var(--color-bg-4);
     color: var(--color-fg-4);
 
-    &:hover {
+    &:hover[disabled=false] {
         background-color: color-mix(in srgb, var(--color-bg-4) 80%, white);
     }
+
+    &[disabled] {
+        background-color: var(--color-bg-disabled);
+        color: var(--color-fg-disabled);
+    }
+`;
+
+const PulseLoaderStyled = styled(PulseLoader)`
+    margin-top: 2px;
 `;
