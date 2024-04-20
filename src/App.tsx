3eebc2aa97b4a708a29542c1b6138231868340e1
Sergio Castelani <sergio.castelani@gmail.com>
2024-04-20 13:34:15 -0300

    feat: give feedback to users about server warmup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import { RouterProvider } from "react-router-dom"
 import './app-style.css'
+import { RouterProvider } from "react-router-dom"
 import { appRouter } from "./app-router";
+import { Toast } from "./components/toast";
+import { useEffect, useRef, useState } from "react";
+import { ApiAuth } from './api/api-auth';
 
 function App() {
+    const [showWarmupMessage, setShowWarmupMessage] = useState(false);
+    const showWarmupMessageRef = useRef<boolean>()
+    showWarmupMessageRef.current = showWarmupMessage;
+
+    const [warmupToastTimeout, setWarmupToastTimeout] = useState(0);
+    const warmupToastTimeoutRef = useRef<number>()
+    warmupToastTimeoutRef.current = warmupToastTimeout;
+
+    useEffect(() => {
+        new ApiAuth()
+            .ping()
+            .then(() => {
+                clearTimeout(warmupToastTimeoutRef.current);
+                if (showWarmupMessageRef.current)
+                    window.history.go(0);
+            })
+            .finally(() => {
+                setShowWarmupMessage(false);
+            });
+
+        setWarmupToastTimeout(setTimeout(() => {
+            setShowWarmupMessage(true);
+        }, 4000));
+    }, []);
+
     return (
-        <RouterProvider router={appRouter} />
+        <>
+            {showWarmupMessage && <Toast text="Servers are warming up. This page will reload automatically when ready."/>}
+            <RouterProvider router={appRouter} />
+        </>
     )
 }
 
